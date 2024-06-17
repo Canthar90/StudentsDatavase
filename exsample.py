3085a7207c7ca82e7636be5c7610cea90dc7715d
Canthar90 <d.cieslak90@gmail.com>
2024-06-17 12:04:11 +0200

    Finishing working layout

--- exsample.py
+++ exsample.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QGridLayout, QLineEdit
+from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton
 import sys
 
 
@@ -7,16 +7,25 @@ class AgeCalculator(QWidget):
         super().__init__()
         grid = QGridLayout()
 
+
+        # Create Widgets
         name_label = QLabel("Name:")
         name_line_edit = QLineEdit()
 
         date_birth_label = QLabel("Date of birth MM/DD/YYYY:")
         date_birth_line_edit = QLineEdit()
 
+        calculate_button = QPushButton("Calculate Age")
+        output_label = QLabel("")
+
+
+        # Add widgets to grid
         grid.addWidget(name_label, 0, 0)
         grid.addWidget(name_line_edit, 0, 1)
         grid.addWidget(date_birth_label, 1, 0)
         grid.addWidget(date_birth_line_edit, 1, 1)
+        grid.addWidget(calculate_button, 2, 0, 1, 2)
+        grid.addWidget(output_label, 3, 0, 1, 2)
 
         self.setLayout(grid)
 
