b5868f3ecb45d3bfcd299a27c952f3a52604cb69
Canthar90 <d.cieslak90@gmail.com>
2024-06-17 11:35:05 +0200

    Initial setup

---
+++ exsample.py
@@ -0,0 +1,20 @@
+from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QGridLayout, QLineEdit
+
+
+class AgeCalculator(QWidget):
+    def __init__(self):
+        grid = QGridLayout()
+
+        name_label = QLabel("Name:")
+        name_line_edit = QLineEdit()
+
+        date_birth_label = QLabel("Date of birth MM/DD/YYYY:")
+        date_birth_line_edit = QLineEdit()
+
+        grid.addWidget(name_label, 0, 0)
+        grid.addWidget(name_line_edit, 0, 1)
+        grid.addWidget(date_birth_label, 1, 0)
+        grid.addWidget(date_birth_line_edit, 1, 1)
+
+
+age_calculator = AgeCalculator()
