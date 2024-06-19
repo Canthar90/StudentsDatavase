bc982f36d03a5adae14aa09616c3ddd62b89e3dd
Canthar90 <d.cieslak90@gmail.com>
2024-06-19 14:20:24 +0200

    Added some data in sql and simple reading data from db

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton, QMainWindow, \
-    QTableWidget
+    QTableWidget, QTableWidgetItem
 from PyQt6.QtGui import QAction
 import sys
+import sqlite3
 
 
 class MainWindow(QMainWindow):
@@ -9,9 +10,11 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle('Student Managment app')
 
+        # Adding menu bar
         file_menu_item = self.menuBar().addMenu('&File')
         help_menu_item = self.menuBar().addMenu('&Help')
 
+        # Adding actions for menu items
         add_student_action = QAction('Add Student', self)
         file_menu_item.addAction(add_student_action)
 
@@ -19,13 +22,25 @@ class MainWindow(QMainWindow):
         help_menu_item.addAction(about_action)
         about_action.setMenuRole(QAction.MenuRole.NoRole)
 
+        # Creating table
         self.table = QTableWidget()
         self.table.setColumnCount(4)
         self.table.setHorizontalHeaderLabels(('ID', 'Name', 'Course', 'Mobile'))
+        self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)
+        self.load_data()
 
+    
     def load_data(self):
-        self.table
+        connection = sqlite3.connect('database.db')
+        result = connection.execute('SELECT * FROM students')
+        self.table.setRowCount(0)
+        for row_number, row_data in enumerate(result):
+            self.table.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))
+
+
 
 app = QApplication(sys.argv)
 student_managment_main = MainWindow()
