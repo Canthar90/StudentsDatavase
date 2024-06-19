83a362a1128433e59ca6752c2e9b2e1960ce8d5e
Canthar90 <d.cieslak90@gmail.com>
2024-06-19 13:13:52 +0200

    Added table and seted it as centralWidget

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton, QMainWindow
+from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton, QMainWindow, \
+    QTableWidget
 from PyQt6.QtGui import QAction
 import sys
 
@@ -17,6 +18,14 @@ class MainWindow(QMainWindow):
         about_action = QAction('About', self)
         help_menu_item.addAction(about_action)
         about_action.setMenuRole(QAction.MenuRole.NoRole)
+
+        self.table = QTableWidget()
+        self.table.setColumnCount(4)
+        self.table.setHorizontalHeaderLabels(('ID', 'Name', 'Course', 'Mobile'))
+        self.setCentralWidget(self.table)
+
+    def load_data(self):
+        self.table
 
 app = QApplication(sys.argv)
 student_managment_main = MainWindow()
